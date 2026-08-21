628573e3c83f8c9407a8ad6c362e12a9be05a3db
Chitransh Saxena <146615780+CHITRANSH1705@users.noreply.github.com>
2026-08-21 14:05:43 +0530

    Update 0 Crack.py

--- 0 Crack.py
+++ 0 Crack.py
@@ -15,7 +15,6 @@ import json
 # ---------------------------
 # CONFIG
 # ---------------------------
-
 MY_PASSWORD = "Chitransh@123"
 ENCRYPTED_FILE = "advanced_forensic_report.enc"
 KEY_FILE = "forensic_key.key"
